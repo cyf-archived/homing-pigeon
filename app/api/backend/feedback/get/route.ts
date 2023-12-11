041c5a3826880a5140fa0b838e3e923241a1f2fb
kjxbyz <kjxbyz@163.com>
2023-12-11 21:28:45 +0800

    feat: add feedback routes

---
+++ app/api/backend/feedback/get/route.ts
@@ -0,0 +1,73 @@
+import httpStatus from "http-status";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+export const dynamic = "force-dynamic";
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    if (!id) {
+      return NextResponse.json(
+        {
+          code: httpStatus.BAD_REQUEST,
+          msg: "Bad request",
+          timestamp: Date.now(),
+        },
+        { status: httpStatus.BAD_REQUEST },
+      );
+    }
+
+    const feedback = await prisma.feedback.findUnique({
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        files: {
+          select: {
+            id: true,
+            feedback_id: true,
+            url: true,
+            type: true,
+            size: true,
+            title: true,
+          },
+          where: {
+            is_del: "NO",
+          },
+        },
+        create_by: true,
+        create_date: true,
+        update_by: true,
+        update_date: true,
+      },
+      where: {
+        id,
+        is_del: "NO",
+      },
+    });
+
+    if (!feedback) {
+      return NextResponse.json(
+        { code: httpStatus.NOT_FOUND, msg: "Not Found", timestamp: Date.now() },
+        { status: httpStatus.NOT_FOUND },
+      );
+    }
+
+    return NextResponse.json(
+      { code: 0, data: feedback, timestamp: Date.now() },
+      { status: httpStatus.OK },
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        code: httpStatus.INTERNAL_SERVER_ERROR,
+        msg: error.message || error.toString(),
+        timestamp: Date.now(),
+      },
+      { status: httpStatus.INTERNAL_SERVER_ERROR },
+    );
+  }
+}
---
+++ app/api/backend/feedback/update/route.ts
@@ -0,0 +1,106 @@
+import httpStatus from "http-status";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { userId } from "@/constants";
+import { schema } from "@/schema/feedback";
+
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+export const dynamic = "force-dynamic";
+
+export async function POST(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    const params = await request.json();
+    const {
+      error,
+      value: { title, description, files = [] },
+    } = schema.validate(params);
+    if (!id || error) {
+      return NextResponse.json(
+        {
+          code: httpStatus.BAD_REQUEST,
+          msg: "Bad request",
+          timestamp: Date.now(),
+        },
+        { status: httpStatus.BAD_REQUEST },
+      );
+    }
+
+    const feedback = await prisma.feedback.update({
+      data: {
+        title,
+        description,
+        files: {
+          updateMany: {
+            data: {
+              is_del: "YES",
+            },
+            where: {
+              id: {
+                notIn: files
+                  .filter((file: any) => !!file.id)
+                  .map((file: any) => file.id),
+              },
+              is_del: "NO",
+            },
+          },
+          createMany: {
+            data: files
+              .filter((file: any) => !file.id)
+              .map(
+                ({
+                  url,
+                  type,
+                  size,
+                  title,
+                }: {
+                  url: string;
+                  type: string;
+                  size: number;
+                  title: string;
+                }) => ({
+                  url,
+                  type,
+                  size,
+                  title,
+                  create_by: userId,
+                  update_by: userId,
+                }),
+              ),
+          },
+        },
+        update_by: userId,
+        update_date: new Date(),
+      },
+      include: {
+        files: true,
+      },
+      where: {
+        id,
+        is_del: "NO",
+      },
+    });
+
+    if (!feedback) {
+      return NextResponse.json(
+        { code: httpStatus.NOT_FOUND, msg: "Not Found", timestamp: Date.now() },
+        { status: httpStatus.NOT_FOUND },
+      );
+    }
+
+    return NextResponse.json(
+      { code: 0, data: feedback, timestamp: Date.now() },
+      { status: httpStatus.OK },
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        code: httpStatus.INTERNAL_SERVER_ERROR,
+        msg: error.message || error.toString(),
+        timestamp: Date.now(),
+      },
+      { status: httpStatus.INTERNAL_SERVER_ERROR },
+    );
+  }
+}
