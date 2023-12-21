7a2d2db874d0513fdbc02d361a9881d484886c72
kjxbyz <kjxbyz@163.com>
2023-12-21 22:34:10 +0800

    fix: update `join-insider`

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -129,7 +129,7 @@ export default function Home({
           />
         </div>
         <h1
-          className="animate-fade-up bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-black/80 opacity-0 drop-shadow-sm dark:text-white/80 md:text-7xl md:leading-[5rem]"
+          className="animate-fade-up bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-black/80 opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem] dark:text-white/80"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
           <Balancer>{t("title")}</Balancer>
@@ -166,7 +166,7 @@ export default function Home({
               </p>
             </Pkg>
             <Link
-              className="flex items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800 dark:bg-black dark:text-white/80 max-md:mx-10"
+              className="flex items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800 max-md:mx-10 dark:bg-black dark:text-white/80"
               href=""
             >
               <GooglePlay className="h-7 w-7" />
@@ -175,7 +175,7 @@ export default function Home({
               </p>
             </Link>
             <Link
-              className="flex items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800 dark:bg-black dark:text-white/80 max-md:mx-10"
+              className="flex items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800 max-md:mx-10 dark:bg-black dark:text-white/80"
               href=""
             >
               <AppStore className="h-7 w-7" />
